483b4c955f250d9b0313ea5f7a850466cb2c8a81
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-12-19 12:20:43 -0500

    router

---
+++ router/movie.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/Leonardo-Antonio/api-movies/handler"
+	"github.com/Leonardo-Antonio/api-movies/storage"
+	"github.com/labstack/echo"
+)
+
+func Movie(movie storage.IMovie, e *echo.Echo) {
+	hand := handler.NewMovie(movie)
+	group := e.Group("/api/v1/movies")
+	group.POST("", hand.Create)
+	group.PUT("", hand.Update)
+	group.DELETE("", hand.Delete)
+	group.GET("/all", hand.GetAll)
+	group.GET("/by/categories/:ID", hand.GetByCategories)
+	group.GET("/by/stars/:stars", hand.GetByStars)
+}
