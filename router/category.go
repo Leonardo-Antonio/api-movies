526ca20da784a0b75a1ecd14de220a2c11d98f03
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-12-20 11:21:04 -0500

    category.go router

--- router/category.go
+++ router/category.go
@@ -1,1 +1,16 @@
 package router
+
+import (
+	"github.com/Leonardo-Antonio/api-movies/handler"
+	"github.com/Leonardo-Antonio/api-movies/storage"
+	"github.com/labstack/echo"
+)
+
+func Category(storage storage.ICategory, e *echo.Echo) {
+	hand := handler.NewCategory(storage)
+	group := e.Group("/api/v1/categories")
+	group.GET("", hand.GetAll)
+	group.POST("", hand.Create)
+	group.PUT("", hand.Update)
+	group.DELETE("/:ID", hand.Delete)
+}
